c4ea9cbd9a597ce31a8b658b8d54d387f5ed60ad
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2023-01-25 20:28:10 -0800

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,20 @@ class Navbar extends Component {
         return (
             <nav className='NavbarItems'>
                 <h1 className='navbar-logo'>Trippy</h1>
+
+                <div className='menu-icons'>
+                    <i className={this.state.clicked ? 'fa fa-times' : 'fa fa-bars'}></i>
+                </div>
+
                 <ul className='nav-menu'>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <a href={item.url}><i className={item.icon}></i>{item.title}</a>
+                                <a href={item.url} className={item.cName}><i className={item.icon}></i>{item.title}</a>
                             </li>
                         )
                     })}
+                    <button>Sign up</button>
                 </ul>
             </nav>
         )
